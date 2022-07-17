9204db3d54e36a7e18032b39bc8eb9fbb9a0398a
SamyakJain2512 <104713466+SamyakJain2512@users.noreply.github.com>
2022-07-17 15:24:22 +0200

    changes to do prediction

--- Models/AutoRec/data_preprocessor.py
+++ Models/AutoRec/data_preprocessor.py
@@ -2,11 +2,15 @@ import numpy as np
 
 def read_rating(path, num_users, num_items,num_total_ratings, a, b, train_ratio):
     fp = open(path + "ratings.dat")
+    fp2 = open(path + "to_predict.dat")
 
     user_train_set = set()
     user_test_set = set()
+    user_val_set = set()
     item_train_set = set()
     item_test_set = set()
+    item_val_set = set()
+
 
     R = np.zeros((num_users,num_items))
     mask_R = np.zeros((num_users, num_items))
@@ -14,9 +18,11 @@ def read_rating(path, num_users, num_items,num_total_ratings, a, b, train_ratio)
 
     train_R = np.zeros((num_users, num_items))
     test_R = np.zeros((num_users, num_items))
+    val_R = np.zeros((num_users, num_items))
 
     train_mask_R = np.zeros((num_users, num_items))
     test_mask_R = np.zeros((num_users, num_items))
+    val_mask_R = np.zeros((num_users, num_items))
 
     random_perm_idx = np.random.permutation(num_total_ratings)
     train_idx = random_perm_idx[0:int(num_total_ratings*train_ratio)]
@@ -58,8 +64,18 @@ def read_rating(path, num_users, num_items,num_total_ratings, a, b, train_ratio)
         user_test_set.add(user_idx)
         item_test_set.add(item_idx)
 
-    return R, mask_R, C, train_R, train_mask_R, test_R, test_mask_R,num_train_ratings,num_test_ratings,\
-user_train_set,item_train_set,user_test_set,item_test_set
+    lines = fp2.readlines()
+    for line in lines:
+        user, item, rating = line.split("::")
+        user_idx = int(user)
+        item_idx = int(item)
+        val_R[user_idx, item_idx] = int(rating)
+        val_mask_R[user_idx, item_idx] = 1
+        user_val_set.add(user_idx)
+        item_val_set.add(item_idx)
+
+    return R, mask_R, C, train_R, train_mask_R, test_R, test_mask_R,val_R,val_mask_R,num_train_ratings,num_test_ratings,\
+user_train_set,item_train_set,user_test_set,item_test_set,user_val_set,item_val_set
 
 
 
